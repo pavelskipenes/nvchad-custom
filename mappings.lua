115a9837306ba2e00970ad693b11f07c46082610
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2023-12-16 19:44:39 +0100

    mappings: remove jump and center
    This is just a buggy mess. The screen keeps jumping between different
    locations making the experience really bad.

--- mappings.lua
+++ mappings.lua
@@ -4,11 +4,6 @@ M.custom = {
     ["<leader>dg"] = { "<cmd> Trouble <CR>", "Diagnostics" },
     ["<leader>z"] = { "<cmd> set wrap! <CR>", "Toggle word wrap" },
     ["<leader>q"] = { "<cmd>q<CR>", "quit" },
-    ["<C-d>"] = { "<C-d>zz", "Jump down" },
-    ["<C-p>"] = { "<C-p>zz", "Jump up" },
-    ["j"] = { "jzz", "down" },
-    ["k"] = { "kzz", "up" },
-    ["<S>g"] = { "<S>gzz", "jump to buttom" },
     ["<leader>K"] = { "<cmd> VisitLinkNearest <CR>", "open link closest to cursor" },
     ["<leader>tt"] = {
       function()
